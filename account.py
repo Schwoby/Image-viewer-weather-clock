561403be5609cb79e793dd38c84f83a2b59f0ba3
Schwoby <65581397+Schwoby@users.noreply.github.com>
2024-04-01 09:34:54 -0500

    Add files via upload
    initial posting

---
+++ account.py
@@ -0,0 +1,9 @@
+class WeatherAccount:
+	account = '' #obtained via https://home.openweathermap.org/api_keys
+	latitude = ''
+	longitude = ''
+
+	# Examples
+	# account = '1234567890abcdef1234567890abcdef'
+	# latitude = '41.875778'
+	# longitude = '-87.618956'
